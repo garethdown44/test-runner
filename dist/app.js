ede1512e87b00a108c96fd136d4465241a9d7bcb
Gareth Down <g@garethdown.com>
2016-01-15 08:20:44 +0000

    init

--- dist/app.js
+++ dist/app.js
@@ -2,80 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('bootstrap');
 
-var cx = require('classnames');
-
-var Tree = require('react-ui-tree');
-
-//var Tree = require('../lib/react-ui-tree.js');
-var tree = require('./tree');
-
-//require('../lib/react-ui-tree.less');
-//require('./theme.less');
-//require('./app.less');
-
-
-var App = React.createClass({
-  getInitialState() {
-    return {
-      active: null,
-      tree: tree
-    };
-  },
-
-  renderNode(node) {
-    return (
-      <span className={cx('node', {
-        'is-active': node === this.state.active
-        })} onClick={this.onClickNode.bind(null, node)}>
-        {node.module}
-      </span>
-    );
-  },
-
-  onClickNode(node) {
-    this.setState({
-      active: node
-    });
-  },
-
-  render() {
-    return (
-      <div className="app">
-        <div className="tree">
-          <Tree
-            paddingLeft={20}
-            tree={this.state.tree}
-            onChange={this.handleChange}
-            isNodeCollapsed={this.isNodeCollapsed}
-            renderNode={this.renderNode}
-          />
-        </div>
-      </div>
-    );
-  },
-
-  handleChange(tree) {
-    this.setState({
-      tree: tree
-    });
-  },
-
-  updateTree() {
-    var tree = this.state.tree;
-    tree.children.push({module: 'test'});
-    this.setState({
-      tree: tree
-    });
-  }
-});
-
-
-
+var Tree = require('./Tree');
 
 
 var Root = React.createClass({
   render: function() {
-    return (<div><div>hello</div><App /></div>);
+    return (<div><div>hello</div><Tree /></div>);
   }
 });
 
---
+++ src/Tree/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Radium from 'radium';
+import {Treebeard, decorators} from '../src/index';
+
+import data from './data';
+import styles from './styles';
+
+const HELP_MSG = 'Select A Node To See Its Data Structure Here...';
+
+// Example: Customising The Header Decorator To Include Icons
+decorators.Header = (props) => {
+    const style = props.style;
+    const iconType = props.node.children ? 'folder' : 'file-text';
+    const iconClass = `fa fa-${iconType}`;
+    const iconStyle = { marginRight: '5px' };
+    return (
+        <div style={style.base}>
+            <div style={style.title}>
+                <i className={iconClass} style={iconStyle}/>
+                {props.node.name}
+            </div>
+        </div>
+    );
+};
+
+class NodeViewer extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const style = styles.viewer;
+        let json = JSON.stringify(this.props.node, null, 4);
+        if(!json){ json = HELP_MSG; }
+        return (
+            <div style={style.base}>
+                {json}
+            </div>
+        );
+    }
+}
+
+NodeViewer.propTypes = {
+    node: React.PropTypes.object
+};
+
+//@Radium
+class DemoTree extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+        this.onToggle = this.onToggle.bind(this);
+    }
+    onToggle(node, toggled){
+        if(this.state.cursor){this.state.cursor.active = false;}
+        node.active = true;
+        if(node.children){ node.toggled = toggled; }
+        this.setState({ cursor: node });
+    }
+    render(){
+        return (
+            <div>
+                <div style={styles.component}>
+                    <Treebeard
+                        data={data}
+                        onToggle={this.onToggle}
+                        decorators={decorators}
+                    />
+                </div>
+                <div style={styles.component}>
+                    <NodeViewer node={this.state.cursor}/>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+module.exports = Radium(DemoTree);
